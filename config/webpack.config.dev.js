597fa4313e2ad6ad3ff17e0df0ead29bc499dff8
Manuel Rodriguez <rodriguez.manu@gmail.com>
2018-08-09 17:21:41 -0300

    Fix automatic eslint

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -60,10 +60,9 @@ module.exports = {
     // There are also additional JS chunk files if you use code splitting.
     chunkFilename: 'static/js/[name].chunk.js',
     // This is the URL that app is served from. We use "/" in development.
-    publicPath: publicPath,
+    publicPath,
     // Point sourcemap entries to original disk location (format as URL on Windows)
-    devtoolModuleFilenameTemplate: info =>
-      path.resolve(info.absoluteResourcePath).replace(/\\/g, '/'),
+    devtoolModuleFilenameTemplate: info => path.resolve(info.absoluteResourcePath).replace(/\\/g, '/'),
   },
   resolve: {
     // This allows you to set a fallback for where Webpack should look for modules.
@@ -72,7 +71,7 @@ module.exports = {
     // https://github.com/facebookincubator/create-react-app/issues/253
     modules: ['node_modules', paths.appNodeModules].concat(
       // It is guaranteed to exist because we tweak it in `env.js`
-      process.env.NODE_PATH.split(path.delimiter).filter(Boolean)
+      process.env.NODE_PATH.split(path.delimiter).filter(Boolean),
     ),
     // These are the reasonable defaults supported by the Node ecosystem.
     // We also include JSX as a common component filename extension to support
@@ -113,7 +112,7 @@ module.exports = {
             options: {
               formatter: eslintFormatter,
               eslintPath: require.resolve('eslint'),
-              emitWarning: true
+              emitWarning: true,
             },
             loader: require.resolve('eslint-loader'),
           },
@@ -201,9 +200,9 @@ module.exports = {
                   importLoaders: 1,
                 },
               },
-                {
-                    loader: require.resolve('sass-loader'),
-                },
+              {
+                loader: require.resolve('sass-loader'),
+              },
               {
                 loader: require.resolve('postcss-loader'),
                 options: {
